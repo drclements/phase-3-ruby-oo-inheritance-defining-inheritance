aa5e23fae662e10232b89924b3469ef2c34749cc
Daniel Clements <drclements@liberty.edu>
2022-12-14 02:44:44 -0500

    commit tree 3b5a3e519e3ea5ed79704a81bdc2cf589f3608b6

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,11 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
+require "pry"
 require_relative './vehicle'
+
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
+
+binding.pry
